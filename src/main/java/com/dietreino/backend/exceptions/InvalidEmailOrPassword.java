b8ca32126ec7915c6949feccd655c317e304964e
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2024-07-08 10:00:57 -0300

    DTR-69: removed unused constructor

--- src/main/java/com/dietreino/backend/exceptions/InvalidEmailOrPassword.java
+++ src/main/java/com/dietreino/backend/exceptions/InvalidEmailOrPassword.java
@@ -1,10 +1,6 @@
 package com.dietreino.backend.exceptions;
 
 public class InvalidEmailOrPassword extends IllegalArgumentException {
-    public InvalidEmailOrPassword(String message) {
-        super(message);
-    }
-
     public InvalidEmailOrPassword() {
         super("E-mail ou senha incorretos");
     }
